7f267ea673a891901d957d37c5323f55fbb4fee3
eytanohana <eohana98@gmail.com>
2022-11-07 02:09:23 +0200

    std db

--- db.py
+++ db.py
@@ -1,48 +1,93 @@
 
 users = [
 	{
-		'name': "Kyle Blackburn",
-		'phone': "1-335-910-4987",
-		'email': "molestie.orci@icloud.net",
-		'address': "P.O. Box 226, 1963 Molestie. Rd.",
-		'postalZip': "83376",
-		'region': "São Paulo",
-		'country': "Vietnam"
+		'name': 'Kyle Blackburn',
+		'phone': '1-335-910-4987',
+		'email': 'molestie.orci@icloud.net',
+		'address': 'P.O. Box 226, 1963 Molestie. Rd.',
+		'postalZip': '83376',
+		'region': 'São Paulo',
+		'country': 'Vietnam'
 	},
 	{
-		'name': "Robert Mclean",
-		'phone': "(438) 851-2678",
-		'email': "donec.at.arcu@hotmail.org",
-		'address': "805-5894 Mauris. Street",
-		'postalZip': "66535",
-		'region': "Guaviare",
-		'country': "South Africa"
+		'name': 'Robert Mclean',
+		'phone': '(438) 851-2678',
+		'email': 'donec.at.arcu@hotmail.org',
+		'address': '805-5894 Mauris. Street',
+		'postalZip': '66535',
+		'region': 'Guaviare',
+		'country': 'South Africa'
 	},
 	{
-		'name': "Stacy Hensley",
-		'phone': "(687) 328-9465",
-		'email': "aliquam.tincidunt.nunc@yahoo.com",
-		'address': "229-9774 Mi Rd.",
-		'postalZip': "77060",
+		'name': 'Stacy Hensley',
+		'phone': '(687) 328-9465',
+		'email': 'aliquam.tincidunt.nunc@yahoo.com',
+		'address': '229-9774 Mi Rd.',
+		'postalZip': '77060',
 		'region': "Provence-Alpes-Côte d'Azur",
-		'country': "Netherlands"
+		'country': 'Netherlands'
 	},
 	{
-		'name': "Brian Calderon",
-		'phone': "(531) 261-5818",
-		'email': "lectus.convallis.est@protonmail.couk",
-		'address': "734-4396 Id, Avenue",
-		'postalZip': "132563",
-		'region': "Central Region",
-		'country': "Singapore"
+		'name': 'Brian Calderon',
+		'phone': '(531) 261-5818',
+		'email': 'lectus.convallis.est@protonmail.couk',
+		'address': '734-4396 Id, Avenue',
+		'postalZip': '132563',
+		'region': 'Central Region',
+		'country': 'Singapore'
 	},
 	{
-		'name': "Winifred Schwartz",
-		'phone': "1-318-626-7840",
-		'email': "velit.eu.sem@outlook.com",
-		'address': "4435 Velit. St.",
-		'postalZip': "8724",
-		'region': "Northern Mindanao",
-		'country': "Italy"
+		'name': 'Winifred Schwartz',
+		'phone': '1-318-626-7840',
+		'email': 'velit.eu.sem@outlook.com',
+		'address': '4435 Velit. St.',
+		'postalZip': '8724',
+		'region': 'Northern Mindanao',
+		'country': 'Italy'
+	},
+	{
+		'name': 'Eugenia Avery',
+		'phone': '(488) 349-6388',
+		'email': 'ac.metus.vitae@icloud.edu',
+		'address': 'Ap #263-5738 Integer Rd.',
+		'postalZip': '67720-119',
+		'region': 'North-East Region',
+		'country': 'China'
+	},
+	{
+		'name': 'Gretchen Lowery',
+		'phone': '(808) 773-4432',
+		'email': 'cursus@google.ca',
+		'address': 'Ap #270-9830 Integer Ave',
+		'postalZip': '2552',
+		'region': 'Ryazan Oblast',
+		'country': 'Germany'
+	},
+	{
+		'name': 'Plato Pratt',
+		'phone': '(363) 295-8555',
+		'email': 'lacus.pede.sagittis@hotmail.ca',
+		'address': '191-1090 Lorem, St.',
+		'postalZip': '36133',
+		'region': 'Hatay',
+		'country': 'Ireland'
+	},
+	{
+		'name': 'George Jacobs',
+		'phone': '(578) 516-1709',
+		'email': 'vulputate@aol.org',
+		'address': 'Ap #674-9998 Diam Street',
+		'postalZip': '735152',
+		'region': 'Kyiv oblast',
+		'country': 'South Africa'
+	},
+	{
+		'name': 'Pamela Washington',
+		'phone': '1-663-651-7137',
+		'email': 'diam.nunc@protonmail.org',
+		'address': '762-9738 Ac Avenue',
+		'postalZip': '538385',
+		'region': 'Troms og Finnmark',
+		'country': 'India'
 	}
 ]
