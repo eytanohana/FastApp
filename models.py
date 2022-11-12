fd5ac19582d0d3ca954ebe886f167e8db4bbc19f
eytanohana <eohana98@gmail.com>
2022-11-12 22:38:47 +0200

    add user sqlmodel

--- models.py
+++ models.py
@@ -1,11 +1,16 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import EmailStr
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
 
 
-class User(BaseModel):
-    name: str
-    phone: str
-    email: EmailStr
-    address: str
-    postalZip: str
-    region: str
-    country: str
+class User(SQLModel, table=True):
+    __tablename__ = 'users'
+
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: Optional[EmailStr] = Field(unique=True)
+    phone_number: Optional[str] = Field(unique=True)
+    zip_code: Optional[str] = None
+    country: Optional[str] = None
+    birthday: Optional[datetime] = None
