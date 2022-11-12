154bfbde2e2771f19dc541b1836b883a5382a06d
eytanohana <eohana98@gmail.com>
2022-11-13 01:52:47 +0200

    separate mode;l

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlmodel import create_engine, Session, select
 from mangum import Mangum
 
-from models import User
+from models import User, UserRead, UserCreate
 
 load_dotenv(Path(__file__).parent / '.env')
 DB_URL = os.environ.get('DB_CONNECTION_URL')
@@ -16,14 +16,14 @@ app = FastAPI()
 aws_lambda_handler = Mangum(app)
 
 
-@app.get('/users', response_model=list[User])
+@app.get('/users', response_model=list[UserRead])
 async def get_users(amount: int = None):
     with Session(engine) as session:
         return session.exec(select(User).limit(amount)).all()
 
 
-@app.post('/users', response_model=User)
-async def add_user(user: User):
+@app.post('/users', response_model=UserRead)
+async def add_user(user: UserCreate):
     with Session(engine) as session:
         try:
             session.add(user)
