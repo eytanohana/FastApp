458c044dcbd5383bf3ae9fd2301a02698c4813c6
eytanohana <eohana98@gmail.com>
2022-11-13 01:39:40 +0200

    separate mode;s

--- models.py
+++ models.py
@@ -4,13 +4,24 @@ from typing import Optional
 from datetime import datetime
 
 
-class User(SQLModel, table=True):
-    __tablename__ = 'users'
-
-    user_id: Optional[int] = Field(default=None, primary_key=True)
+class UserBase(SQLModel):
     name: str = Field(index=True)
     email: Optional[EmailStr] = Field(unique=True)
     phone_number: Optional[str] = Field(unique=True)
     zip_code: Optional[str] = None
     country: Optional[str] = None
     birthday: Optional[datetime] = None
+
+
+class User(UserBase, table=True):
+    __tablename__ = 'users'
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserRead(UserBase):
+    user_id: int
+
