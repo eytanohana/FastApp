cc93a7641173e203cf4a84445271a4a8e872a501
eytanohana <eohana98@gmail.com>
2022-11-13 02:25:37 +0200

    use session as dependency

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, status, Query
+from fastapi import FastAPI, HTTPException, status, Query, Depends
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import create_engine, Session, select
 from mangum import Mangum
@@ -16,29 +16,31 @@ app = FastAPI()
 aws_lambda_handler = Mangum(app)
 
 
-@app.get('/users', response_model=list[UserRead])
-def get_users(amount: int = Query(default=10, le=100)):
+def get_db_session():
     with Session(engine) as session:
-        return session.exec(select(User).limit(amount)).all()
+        yield session
+
+
+@app.get('/users', response_model=list[UserRead])
+def get_users(session: Session = Depends(get_db_session), amount: int = Query(default=10, le=100)):
+    return session.exec(select(User).limit(amount)).all()
 
 
 @app.get('/users/{user_id}', response_model=UserRead)
-def get_user_by_id(user_id: int):
-    with Session(engine) as session:
-        user = session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
-        return user
+def get_user_by_id(*, session: Session = Depends(get_db_session), user_id: int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    return user
 
 
 @app.post('/users', response_model=UserRead)
-async def add_user(user: UserCreate):
-    with Session(engine) as session:
-        try:
-            user_db = User.from_orm(user)
-            session.add(user_db)
-            session.commit()
-        except IntegrityError:
-            raise HTTPException(status.HTTP_400_BAD_REQUEST, detail='User already taken')
-        session.refresh(user_db)
-        return user_db
+def add_user(*, session: Session = Depends(get_db_session), user: UserCreate):
+    try:
+        user_db = User.from_orm(user)
+        session.add(user_db)
+        session.commit()
+    except IntegrityError:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail='User already taken')
+    session.refresh(user_db)
+    return user_db
