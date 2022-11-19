1f25ed724982b272b18386ccf719f533f7d10738
eytanohana <eohana98@gmail.com>
2022-11-19 21:15:43 +0200

    set rootpath for aws

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mangum import Mangum
 from routers.users import users_router
 
 
-app = FastAPI()
+app = FastAPI(root_path='/dev')
 aws_lambda_handler = Mangum(app)
 
 app.include_router(users_router)
